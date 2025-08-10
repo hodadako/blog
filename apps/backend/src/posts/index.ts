30999edd897ae715105c5534693666f48ad85dee
hodako <kbry12@naver.com>
2025-08-10 17:09:13 +0900

    chore: use zod for dtos

--- apps/backend/src/posts/index.ts
+++ apps/backend/src/posts/index.ts
@@ -2,5 +2,3 @@ export * from './posts.module';
 export * from './posts.controller';
 export * from './posts.service';
 export * from './entities/post.entity';
-export * from './dto/create-post.dto';
-export * from './dto/update-post.dto'; 
--- apps/backend/src/posts/posts.service.ts
+++ apps/backend/src/posts/posts.service.ts
@@ -1,78 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { EntityManager } from '@mikro-orm/core';
-import { Post } from './entities/post.entity';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { Injectable } from '@nestjs/common';
+import {EntityRepository} from '@mikro-orm/core';
+import {Post} from "./entities/post.entity";
 
 @Injectable()
 export class PostsService {
-  constructor(private readonly em: EntityManager) {}
-
-  async create(createPostDto: CreatePostDto): Promise<Post> {
-    const post = this.em.create(Post, {
-      ...createPostDto,
-      publishedAt: createPostDto.publishedAt ? new Date(createPostDto.publishedAt) : undefined,
-      isPublished: createPostDto.isPublished ?? false,
-    });
-    
-    await this.em.persistAndFlush(post);
-    return post;
-  }
-
-  async findAll(): Promise<Post[]> {
-    return await this.em.find(Post, {}, { orderBy: { createdAt: 'DESC' } });
-  }
-
-  async findPublished(): Promise<Post[]> {
-    return await this.em.find(Post, { isPublished: true }, { orderBy: { publishedAt: 'DESC' } });
-  }
-
-  async findOne(id: number): Promise<Post> {
-    const post = await this.em.findOne(Post, { id });
-    if (!post) {
-      throw new NotFoundException(`Post with ID ${id} not found`);
-    }
-    return post;
-  }
-
-  async findBySlug(slug: string): Promise<Post> {
-    const post = await this.em.findOne(Post, { slug });
-    if (!post) {
-      throw new NotFoundException(`Post with slug ${slug} not found`);
-    }
-    return post;
-  }
-
-  async update(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
-    const post = await this.findOne(id);
-    
-    this.em.assign(post, {
-      ...updatePostDto,
-      publishedAt: updatePostDto.publishedAt ? new Date(updatePostDto.publishedAt) : post.publishedAt,
-    });
-    
-    await this.em.flush();
-    return post;
-  }
-
-  async remove(id: number): Promise<void> {
-    const post = await this.findOne(id);
-    await this.em.removeAndFlush(post);
-  }
-
-  async publish(id: number): Promise<Post> {
-    const post = await this.findOne(id);
-    post.isPublished = true;
-    post.publishedAt = new Date();
-    await this.em.flush();
-    return post;
-  }
-
-  async unpublish(id: number): Promise<Post> {
-    const post = await this.findOne(id);
-    post.isPublished = false;
-    post.publishedAt = undefined;
-    await this.em.flush();
-    return post;
-  }
+  constructor(private readonly em: EntityRepository<Post>) {}
 } 
