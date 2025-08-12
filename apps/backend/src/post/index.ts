7c7c0bf36e54c701544b8a7bdb2635e442904b3c
hodako <kbry12@naver.com>
2025-08-12 18:13:25 +0900

    chore: fix tsconfig1

--- apps/backend/src/post/index.ts
+++ apps/backend/src/post/index.ts
@@ -4,3 +4,4 @@ export { PostService } from './post.service';
 export { Post } from './entity/post.entity';
 export { Tag } from './entity/tag.entity';
 export { PostTag } from './entity/post-tag.entity';
+export { CreatePostRequest } from '@schema/post/post.request'
