1af84af31f4328d7a26bff408bc7cba12467cccb
hodako <kbry12@naver.com>
2025-08-26 18:35:20 +0900

    feat: deleted useless fields in post.entity.ts

--- apps/backend/src/post/entity/post.entity.ts
+++ apps/backend/src/post/entity/post.entity.ts
@@ -1,40 +1,29 @@
 import {
-  Entity,
-  PrimaryKey,
-  Property,
-  Index,
-  OneToMany,
-  Collection,
+    Entity,
+    PrimaryKey,
+    Property,
+    OneToMany,
+    Collection,
 } from '@mikro-orm/core';
-import { PostTag } from './post-tag.entity';
+import {PostTag} from './post-tag.entity';
 
-@Entity({ tableName: 'posts' })
+@Entity({tableName: 'posts'})
 export class Post {
-  @PrimaryKey()
-  id!: number;
+    @PrimaryKey()
+    id!: number;
 
-  @Property({ length: 255 })
-  @Index()
-  title!: string;
+    @Property()
+    createdAt: Date = new Date();
 
-  @Property()
-  description!: string;
+    @Property({onUpdate: () => new Date()})
+    updatedAt: Date = new Date();
 
-  @Property({ type: 'text' })
-  content!: string;
+    @Property({default: false})
+    isPublished: boolean = false;
 
-  @Property()
-  createdAt: Date = new Date();
+    @Property({length: 255, nullable: true})
+    slug?: string;
 
-  @Property({ onUpdate: () => new Date() })
-  updatedAt: Date = new Date();
-
-  @Property({ default: false })
-  isPublished: boolean = false;
-
-  @Property({ length: 255, nullable: true })
-  slug?: string;
-
-  @OneToMany(() => PostTag, (pt) => pt.post)
-  tags = new Collection<PostTag>(this);
+    @OneToMany(() => PostTag, (pt) => pt.post)
+    tags = new Collection<PostTag>(this);
 }
