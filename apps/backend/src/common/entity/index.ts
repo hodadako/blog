cc2afac0d3a9812e3afa628ddcbc7d4c9f03dadb
hodako <kbry12@naver.com>
2025-08-31 17:04:08 +0900

    fix: changed paths to base entity

---
+++ apps/backend/src/common/entity/index.ts
@@ -0,0 +1,3 @@
+export {BaseEntity} from './base.entity';
+export {Language} from './language.enum';
+export {Translatable} from './translatable.interface';
--- apps/backend/src/common/index.ts
+++ apps/backend/src/common/index.ts
@@ -1,3 +1,1 @@
-export { BaseEntity } from './entity/base.entity';
-export { Translatable } from './entity/translatable.interface';
-export { Language } from './entity/language.enum';
+export {BaseEntity, Language, Translatable} from './entity';
