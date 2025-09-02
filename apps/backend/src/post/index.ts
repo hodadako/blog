04b622835016b3d03655cfbc6f8d8d85cf04d9e5
hodako <kbry12@naver.com>
2025-09-02 19:08:00 +0900

    chore: added paths to repositories in index.ts

--- apps/backend/src/post/index.ts
+++ apps/backend/src/post/index.ts
@@ -1,8 +1,8 @@
-export { PostModule } from './post.module';
-export { PostController } from './post.controller';
-export { PostService } from './post.service';
-export { PostRepository } from './post.repository';
-export { Post } from './domain/post.entity';
-export { Tag } from './domain/tag.entity';
-export { PostTag } from './domain/post-tag.entity';
-export { PostContent } from './domain/post-content.entity';
+export {PostModule} from './post.module';
+export {PostService} from './application/post.service';
+export {PostRepository} from './application/required/post.repository.port';
+export {PostRepositoryImpl} from './adapter/persistence/post.repository.adapter'
+export {Post} from './domain/post.entity';
+export {Tag} from './domain/tag.entity';
+export {PostTag} from './domain/post-tag.entity';
+export {PostContent} from './domain/post-content.entity';
