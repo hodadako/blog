7c1e039f25ae2753fc475808447e43f66048d340
hodako <kbry12@naver.com>
2025-09-02 19:27:15 +0900

    feat: added method to create post

--- apps/backend/src/post/application/post.service.ts
+++ apps/backend/src/post/application/post.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
 import {CreatePostRequest} from "@schema/post";
+import {Post, PostCreate, PostRepository} from "@backend/post";
 
 @Injectable()
-export class PostService {
-  create(request: CreatePostRequest) {
-    
-  }
+export class PostService implements PostCreate {
+    constructor(
+        @Inject()
+        private readonly postRepository: PostRepository
+    ) {
+    }
+
+    async create(createPostRequest: CreatePostRequest): Promise<Post> {
+        return this.postRepository.create(Post.create(createPostRequest));
+    }
+
 }
--- apps/backend/src/post/index.ts
+++ apps/backend/src/post/index.ts
@@ -1,5 +1,6 @@
 export {PostModule} from './post.module';
 export {PostService} from './application/post.service';
+export {PostCreate} from './application/provided/post.create'
 export {PostRepository} from './application/required/post.repository.port';
 export {PostRepositoryImpl} from './adapter/persistence/post.repository.adapter'
 export {Post} from './domain/post.entity';
