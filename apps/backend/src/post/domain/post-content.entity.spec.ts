5cfc48d3cb829ee30b9c6a22743b1bf62bf3f43f
hodako <kbry12@naver.com>
2025-09-07 23:40:54 +0900

    test: added unit test for post-content, and added test path for jest

---
+++ apps/backend/src/post/domain/post-content.entity.spec.ts
@@ -0,0 +1,22 @@
+import { PostContent } from '@backend/post';
+import { PostContentCreateRequest } from '@schema/post';
+
+describe('PostContent Entity', () => {
+  describe('create', () => {
+    it('should set the isPublished field to true by default upon creation', () => {
+      const request: PostContentCreateRequest = {
+        title: 'Test Title',
+        description: 'Test Description',
+        content: 'This is the test content.',
+        language: 'English',
+        slug: 'test-slug',
+      };
+
+      const postContent = PostContent.create(request);
+
+      expect(postContent.isPublished).toBe(true);
+    });
+  });
+});
+
+
