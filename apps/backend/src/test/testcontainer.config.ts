4be847b72a360d3a19806cb9c42faf4fd39668c1
hodako <kbry12@naver.com>
2025-09-10 18:53:42 +0900

    chore: testcontainer settings

---
+++ apps/backend/src/test/testcontainer.config.ts
@@ -0,0 +1,18 @@
+import { MySqlContainer, StartedMySqlContainer } from '@testcontainers/mysql';
+
+let container: StartedMySqlContainer;
+
+export const startMySqlContainer = async () => {
+  container = await new MySqlContainer("mysql:8.0.28")
+    .withDatabase('test')
+    .withUsername('test-user')
+    .withUserPassword('test-password')
+    .start();
+  return container;
+};
+
+export const stopMySqlContainer = async () => {
+  if (container) {
+    await container.stop();
+  }
+};
