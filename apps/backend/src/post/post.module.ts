c5394581cbd1889b54128c736badea91ce5f6bc6
hodako <kbry12@naver.com>
2025-09-12 23:31:42 +0900

    feat: added new providers to post module

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -1,16 +1,20 @@
-import { Module } from '@nestjs/common';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { Post } from '@backend/post/domain/post.entity';
-import { PostService } from '@backend/post/application/post.service';
-import { PostRepositoryImpl } from '@backend/post/adapter/persistence/post.repository.adapter';
-import { PostRepository } from '@backend/post/application/required/post.repository.port';
+import {Module} from '@nestjs/common';
+import {MikroOrmModule} from '@mikro-orm/nestjs';
+import {Post} from '@backend/post/domain/post.entity';
+import {PostModifyService} from '@backend/post/application/post.modify.service';
+import {PostRepositoryImpl} from '@backend/post/adapter/persistence/post.repository.adapter';
+import {PostRepository} from '@backend/post/application/required/post.repository.port';
+import {PostContentRepository} from '@backend/post/application/required/post-content.repository.port';
+import {PostContentRepositoryImpl} from "@backend/post/adapter/persistence/post-content.repository.adapter";
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Post])],
-  providers: [
-    PostService,
-    { provide: PostRepository, useClass: PostRepositoryImpl },
-  ],
-  exports: [PostService],
+    imports: [MikroOrmModule.forFeature([Post])],
+    providers: [
+        PostModifyService,
+        {provide: PostRepository, useClass: PostRepositoryImpl},
+        {provide: PostContentRepository, useClass: PostContentRepositoryImpl},
+    ],
+    exports: [PostModifyService],
 })
-export class PostModule {}
+export class PostModule {
+}
