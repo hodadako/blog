935c496bbf336787cf3575cee38f350e8a17f764
hodako <kbry12@naver.com>
2025-09-15 23:58:43 +0900

    feat: added missing entities in post module

--- apps/backend/src/post/post.module.ts
+++ apps/backend/src/post/post.module.ts
@@ -6,9 +6,12 @@ import {PostRepositoryImpl} from '@backend/post/adapter/persistence/post.reposit
 import {PostRepository} from '@backend/post/application/required/post.repository.port';
 import {PostContentRepository} from '@backend/post/application/required/post-content.repository.port';
 import {PostContentRepositoryImpl} from "@backend/post/adapter/persistence/post-content.repository.adapter";
+import {PostContent} from "@backend/post/domain/post-content.entity";
+import {Tag} from "@backend/post/domain/tag.entity";
+import {PostTag} from "@backend/post/domain/post-tag.entity";
 
 @Module({
-    imports: [MikroOrmModule.forFeature([Post])],
+    imports: [MikroOrmModule.forFeature([Post, PostContent, Tag, PostTag])],
     providers: [
         PostModifyService,
         {provide: PostRepository, useClass: PostRepositoryImpl},
