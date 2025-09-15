fc5757f1dfbdc53e881d4d3344df227a9de80263
hodako <kbry12@naver.com>
2025-09-15 23:58:20 +0900

    fix: changed method not to use entity manager

--- apps/backend/src/post/adapter/persistence/post-content.repository.adapter.ts
+++ apps/backend/src/post/adapter/persistence/post-content.repository.adapter.ts
@@ -1,8 +1,8 @@
-import { PostContentRepository } from '@backend/post/application/required/post-content.repository.port';
-import { PostContent } from '@backend/post';
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/core';
+import {PostContentRepository} from '@backend/post/application/required/post-content.repository.port';
+import {PostContent} from '@backend/post';
+import {Injectable} from '@nestjs/common';
+import {InjectRepository} from '@mikro-orm/nestjs';
+import {EntityRepository} from '@mikro-orm/core';
 
 @Injectable()
 export class PostContentRepositoryImpl implements PostContentRepository {
@@ -12,8 +12,6 @@ export class PostContentRepositoryImpl implements PostContentRepository {
   ) {}
 
   async create(postContent: PostContent): Promise<PostContent> {
-    const created = this.postContentRepository.create(postContent);
-    this.postContentRepository.getEntityManager().persist(created);
-    return created;
+    return this.postContentRepository.create(postContent);
   }
 }
