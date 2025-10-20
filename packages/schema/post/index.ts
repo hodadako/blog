f4a105b901d09e5d995efc215b43cc5311023fde
hodako <kbry12@naver.com>
2025-10-20 11:47:18 +0900

    chore: deleted unused barrel lines

--- packages/schema/post/index.ts
+++ packages/schema/post/index.ts
@@ -1,8 +1,6 @@
-export {postSchema} from './post.schema';
 export {CreatePostRequest} from './post.request';
 export {GetPostResponse, GetPostListResponse} from './post.response';
 export {createPostRequestFixture} from './post.fixture';
 
-export {postContentSchema} from './post-content.schema';
 export {CreatePostContentRequest} from './post-content.request';
 export {createPostContentRequestFixture} from './post-content.fixture';
