d23b7929c96d599cad2632ec80b53cc8feac0c03
hodako <kbry12@naver.com>
2025-12-28 18:50:24 +0900

    style: lint checked

--- apps/backend/src/comment/comment.module.ts
+++ apps/backend/src/comment/comment.module.ts
@@ -3,13 +3,10 @@ import { Module } from '@nestjs/common';
 import { CommentRepository } from '@backend/comment/application/required/comment.repository.port';
 import { MikroCommentRepository } from '@backend/comment/application/provided/persistence/comment.repository.adapter';
 import { Comment } from '@backend/comment/domain/comment.entity';
-import {PostModule} from "@backend/post/post.module";
+import { PostModule } from '@backend/post/post.module';
 
 @Module({
-  imports: [
-      MikroOrmModule.forFeature([Comment]),
-      PostModule,
-  ],
+  imports: [MikroOrmModule.forFeature([Comment]), PostModule],
   providers: [{ provide: CommentRepository, useClass: MikroCommentRepository }],
 })
 export class CommentModule {}
