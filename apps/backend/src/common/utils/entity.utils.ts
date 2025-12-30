920b33745f5cc6f88afa8219f8de6ef1be3b73d4
hodako <kbry12@naver.com>
2025-12-30 10:36:36 +0900

    refactor: changed throwIfNull to deal with undefined

--- apps/backend/src/common/utils/entity.utils.ts
+++ apps/backend/src/common/utils/entity.utils.ts
@@ -1,19 +1,19 @@
 import { NotFoundException } from '@nestjs/common';
-
+//TODO: 예외를 서비스 전용 예외로 변경하기, 아마 이름도 변경해야할 듯, 함수 명도 requireNonNull 같은 걸로 (Java 스타일로)
 export async function throwIfNull<T, E extends new (message: string) => Error>(
-  promise: Promise<T | null>,
+  promise: Promise<T | null | undefined>,
   errorMessage: string,
   ErrorClass: E,
 ): Promise<T> {
   const result = await promise;
-  if (!result) {
+  if (result == null) {
     throw new ErrorClass(errorMessage);
   }
   return result;
 }
 
 export async function throwNotFoundExceptionIfNull<T>(
-  promise: Promise<T | null>,
+  promise: Promise<T | null | undefined>,
   errorMessage: string,
 ): Promise<T> {
   return throwIfNull(promise, errorMessage, NotFoundException);
