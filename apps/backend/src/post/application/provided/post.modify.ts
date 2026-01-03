c73a845b137c2f4c3c14fb2d88a2c1a193977370
hodako <kbry12@naver.com>
2026-01-03 19:46:25 +0900

    chore: renamed post.create.ts to post.modify.ts

--- apps/backend/src/post/application/provided/post.modify.ts
+++ apps/backend/src/post/application/provided/post.modify.ts
@@ -1,6 +1,6 @@
 import { CreatePostRequest } from '@schema/post';
 import { Post } from '@backend/post';
 
-export abstract class PostCreate {
+export abstract class PostModify {
   abstract create(createPostRequest: CreatePostRequest): Promise<Post>;
 }
